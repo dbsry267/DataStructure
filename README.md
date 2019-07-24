ba56065f44868b87bf57fbc31d120bc045380254
Yunkyo Jung <uyuwhiteuyu@gmail.com>
2019-07-24 22:48:08 +0900

    Stack 설명 추가

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # DataStructure
 Data Structure 공부를 위한 Repository
+## 개요
+### 사용언어 : C
+### - 공부내용
+#### 1. Stack
+#### 2. Queue
+#### 3. LinkedList
+## 1) Stack
+---
--- datastructure/stack.h
+++ datastructure/stack.h
@@ -13,12 +13,25 @@ struct stack{
 
 typedef struct stack Stack;
 
+// 스택을 생성하는 함수
 extern Stack stack_init(int size);
+
+// 스택이 비었는지 여부를 0과 1로 반환
 extern int stack_empty(Stack* s);
+
+// 스택에 새로운 값을 추가
 extern int stack_push(Stack* s, char item);
+
+// 스택 맨위의 값을 제거
 extern int stack_pop(Stack* s);
+
+// 스택 맨위의 값을 가져옮
 extern char stack_top(Stack* s);
+
+// 스택을 제거
 extern void stack_delete(Stack* s);
+
+// 스택의 크기를 반환
 extern int stack_size(Stack* s);
 
 #endif //DATASTRUCTURE_STACK_H
